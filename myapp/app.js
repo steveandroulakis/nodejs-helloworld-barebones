3cfa6e8ebeb9ad39e4d258ec6359092525e6e6c1
Androulakis <androula@amazon.com>
2022-06-16 17:04:29 -0700

    changed text and test

--- myapp/app.js
+++ myapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Steve\'s test deployment')
+  res.send("<strong>Steve's</strong> server has started.")
 })
 
 module.exports = app
